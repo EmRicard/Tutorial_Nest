871547a53141305046e2134d73ed1d289d9deadf
Ricardo Cedraz <emanuel.cedrazta@hotmail.com>
2026-02-05 15:29:18 -0300

    first commit

---
+++ src/task/task.controller.ts
@@ -0,0 +1,25 @@
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { TaskDto } from './task.dto';
+import { TaskService } from './task.service';
+
+@Controller('task')
+export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
+
+  @Post()
+  create(@Body() task: TaskDto) {
+    this.taskService.create(task);
+    return task;
+  }
+
+  @Get('/:id')
+  findById(@Param('id') id: string): TaskDto {
+    return this.taskService.findById(id);
+  }
+
+  @Put('/:id')
+  update(@Body() task: TaskDto) {
+    this.taskService.update(task);
+    return task;
+  }
+}
---
+++ src/task/task.service.ts
@@ -0,0 +1,38 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { TaskDto } from './task.dto';
+
+@Injectable()
+export class TaskService {
+  private tasks: TaskDto[] = [];
+
+  create(task: TaskDto) {
+    this.tasks.push(task);
+  }
+
+  findById(id: string): TaskDto {
+    const foundTask = this.tasks.filter((t) => t.id === id);
+
+    if (foundTask.length) {
+      return foundTask[0];
+    }
+
+    throw new HttpException(
+      `Task with id ${id} not found`,
+      HttpStatus.NOT_FOUND,
+    );
+  }
+
+  update(task: TaskDto) {
+    const index = this.tasks.findIndex((t) => t.id === task.id);
+
+    if (index !== -1) {
+      this.tasks[index] = task;
+      return task;
+    }
+
+    throw new HttpException(
+      `Task with id ${task.id} not found`,
+      HttpStatus.BAD_REQUEST,
+    );
+  }
+}
